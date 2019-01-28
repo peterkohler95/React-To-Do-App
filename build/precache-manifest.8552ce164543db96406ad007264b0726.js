94ce845baa1d25f73dd529a7156bbc92dfb31025
Peter K <peterkohler95@gmail.com>
2019-01-28 15:07:02 -0600

    added gh-pages package

---
+++ build/precache-manifest.8552ce164543db96406ad007264b0726.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "39037ce6d62ef694ffd3",
+    "url": "/React-To-Do-App/static/js/runtime~main.39037ce6.js"
+  },
+  {
+    "revision": "2c27c2ff3810f5faa450",
+    "url": "/React-To-Do-App/static/js/main.2c27c2ff.chunk.js"
+  },
+  {
+    "revision": "ad961163a52a17acddf4",
+    "url": "/React-To-Do-App/static/js/1.ad961163.chunk.js"
+  },
+  {
+    "revision": "2c27c2ff3810f5faa450",
+    "url": "/React-To-Do-App/static/css/main.a3316040.chunk.css"
+  },
+  {
+    "revision": "13244c967a6041f7c118d9197db68405",
+    "url": "/React-To-Do-App/index.html"
+  }
+];
